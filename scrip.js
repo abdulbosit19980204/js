8d3f291d7f955a127975b64e1ada13fba1b41576
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-10-04 05:04:41 +0500

    scroll and mobile menus are showed with black color and changed navigation menu icon color

--- scrip.js
+++ scrip.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
+
     window.addEventListener('scroll', function() {
         const header = document.querySelector('header');
         header.classList.toggle('sticky', window.scrollY > 0)
@@ -6,13 +7,14 @@ window.addEventListener('DOMContentLoaded', function() {
 
     const menuBtn = document.querySelector('.menu-btn')
     const navigation = document.querySelector('.navigation')
-    const navigationItem = document.querySelectorAll('.navigation a')
+    const navigationItems = document.querySelectorAll('.navigation a')
 
     menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('active')
         navigation.classList.toggle('active')
     })
-    navigationItem.forEach(navItem => {
+
+    navigationItems.forEach(navItem => {
         navItem.addEventListener('click', () => {
             menuBtn.classList.remove('active')
             navigation.classList.remove('active')
